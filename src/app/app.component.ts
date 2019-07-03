f02e4def37f34120619429bdd12da6142c482672
Corentin Chataignon <corentin.chataignon44@gmail.com>
2019-07-03 12:05:55 +0200

    Done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'TP-Flickr';
+  copyright = '@Corentin Chataignon';
+  tag = '';
+
+  key = "cbd044cdcfa75f68ef13877b22fe42ff";
+  photos: any;
+
+  constructor(private http: HttpClient) {
+
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('An error occured', error);
+    return Promise.reject(error.message || error);
+  }
+
+  getPhotos(): Promise<any> {
+    let url = "https://api.flickr.com/services/rest?nojsoncallback=1&method=flickr.photos.search&api_key="+this.key+"&tags="+this.tag+"&format=json";
+    
+    return this.http.get(url)
+      .toPromise()
+      .catch(this.handleError);
+  }
+
+  new_tag(): void {
+    this.getPhotos().then(resp => this.photos = resp.photos);
+  }
+
+  getPhotoUrl(photo: any): string {
+    return `https://farm${photo.farm}.static.flickr.com/${photo.server}/${photo.id}_${photo.secret}_s.jpg`;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { PhotoComponent } from './photo/photo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PhotoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
